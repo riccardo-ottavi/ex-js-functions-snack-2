f21897e1116042b8d0d687335974796f0cdd97bc
riccardo-ottavi <ottavi.ps@libero.it>
2025-12-11 16:06:36 +0100

    risolvi ex-1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("prova")
+//SNACK 1--------------
+
+//funzioni anonime dichiarate prima dell'invocazione
+const sumAnonima = (a, b) => {
+    return a + b
+}
+const sumArrow = (a, b) => a + b
+
+console.log(sum(3,5))
+console.log(sumAnonima(30,50))
+console.log(sumArrow(300,500))
+
+function sum(a, b){
+    return a + b
+}
+
+
+//SNACK 2--------------
+
